f1b0a94a00bc8c7ee46a9e64b69adc4bcd2ab3aa
Helder Moreno Lopese <heldermorenolopes@gmail.com>
2020-01-11 12:36:31 +0000

    last updates from the last two days

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'FacilityHack - Building Managment';
+app.locals.title = 'FacilityHack';
 
 
 
